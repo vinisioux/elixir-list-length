bed111ee2971fa929d3676023b39b1c05f82e87c
ViniciusLeonadovky <vile.pb@gmail.com>
2021-07-11 13:28:25 -0300

    done

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    list_length(list, 0)
+  end
+
+  defp list_length([], acc) do
+    acc
+  end
+
+  defp list_length([_head | tail], acc) do
+    acc = acc + 1
+    list_length(tail, acc)
   end
 end
